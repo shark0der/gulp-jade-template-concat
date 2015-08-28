b0defe3827e0371cd316e50ed02f5b67f90b7bc0
shark0der <tolean.dj@gmail.com>
2015-08-28 10:45:28 +0300

    removed console.log call, proper default var fallback, code style fixes

--- index.js
+++ index.js
@@ -2,44 +2,50 @@ var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var through = require('through');
 
-
 function pluginError (message) {
-  return new PluginError('gulp-jade-template-concat', message)
+  return new PluginError('gulp-jade-template-concat', message);
 }
 
-
 module.exports = function jadeConcat(fileName, _opts) {
-  if (!fileName) throw pluginError('Missing fileName')
 
-  console.log(templateVariable);
-  var defaults = {templateVariable: "templates"}
+  if (!fileName) {
+    throw pluginError('Missing filename');
+  }
+
   var concatString = "";
-
+  var templateVariable = _opts.templateVariable || 'templates';
 
   function write (file) {
-    if (file.isNull()) return
-    if (file.isStream()) return this.emit('error', pluginError('Streaming not supported'))
+
+    if (file.isNull()) {
+      return;
+    }
+
+    if (file.isStream()) {
+      return this.emit('error', pluginError('Streaming not supported'));
+    }
 
     //isolate filename from full path
     var filename = file.path.replace(file.base, "").replace(".js", "");
 
     // replace template name with filename
-    var contents = file.contents.toString().replace('function template', '"' + filename + '": function')
+    var contents = file.contents.toString().replace('function template', '"' + filename + '": function');
 
-    concatString += contents + ",\n";;
+    concatString += contents + ",\n";
   }
 
   function end () {
+
     //wrap concatenated string in template object
-    var templateString = "var " + _opts.templateVariable + " = {\n" + concatString + "}";
+    var templateString = "var " + templateVariable + " = {\n" + concatString + "}";
 
     this.queue(new gutil.File({
       path: fileName,
       contents: new Buffer(templateString)
     }))
 
-    this.queue(null)
+    this.queue(null);
   }
 
-  return through(write, end)
+  return through(write, end);
 }
